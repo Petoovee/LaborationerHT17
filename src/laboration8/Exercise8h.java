ad81234c89ae08a5dab58e461c1e5c91a9dc0830
Petoovee <Petomatick@gmail.com>
2018-05-19 14:10:46 +0200

    Exercise8h

--- src/laboration8/Exercise8h.java
+++ src/laboration8/Exercise8h.java
@@ -1,6 +1,6 @@
 package laboration8;
 
-public class PointTest {
+public class Exercise8h {
 	public static void main(String[] args) {
 		Point point1 = new Point(10, 12);
 		Point point2 = new Point(point1);
